a72c1c96df12d8cffb9312f0c741157a590e984b
Kadek Cahya <kdkchy@gmail.com>
2020-09-13 16:51:09 +0700

    pertemuan 2

---
+++ 02/latihan.py
@@ -0,0 +1,33 @@
+print("Hello, Wordl!")
+
+a = 2
+print(type(a))
+
+a = "2"
+print(type(a))
+
+a = 2.0
+print(type(a))
+
+a = {}
+print(type(a))
+
+a = []
+print(type(a))
+
+a.append(1)
+a.append(2)
+a.append(3)
+
+print(a)
+
+print(3 ** 3)
+print(3 * 3)
+print(3 == 3)
+
+nama = "Kadek"
+angka = 8
+
+print("Halo, saya %s, saya memilih angka %d " % (nama,angka))
+
+
