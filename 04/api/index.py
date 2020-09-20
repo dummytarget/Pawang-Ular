240b1744a5452d8b9d7be4dd1ecee493f672a154
Kadek Cahya <kdkchy@gmail.com>
2020-09-20 17:01:25 +0700

    pertemuan 4

---
+++ 04/api/index.py
@@ -0,0 +1,8 @@
+import simple
+
+get = simple.get()
+put = simple.put()
+post = simple.post()
+delete = simple.delete()
+
+print(put)
---
+++ 04/api/simple.py
@@ -0,0 +1,11 @@
+def get():
+    return "Ambil Data"
+
+def put():
+    return "Update Data"
+
+def post():
+    return "Buat Data"
+
+def delete():
+    return "Hapus Data"
