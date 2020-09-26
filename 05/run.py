189309104ecb33e5a186558a45098eff62c70cc6
Kadek Cahya <kdkchy@gmail.com>
2020-09-26 16:35:50 +0700

    jinja

--- 05/run.py
+++ 05/run.py
@@ -5,15 +5,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, Wordl!'
-    #return render_template('home.html', title='Home')
+    user = "Ari" 
+    return render_template('home.html', title='Home', user=user)
+
+@app.route('/profil')
+def profil():
+    user = "Ari" 
+    return render_template('profile.html', title='Profil', user=user)
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-# langkah
-# 1. jalankan virtual environment
-# 2. install flask melalui pip
-# 3. buat file run.py
-# 4. export file tersebut
-# 5. jalankan
