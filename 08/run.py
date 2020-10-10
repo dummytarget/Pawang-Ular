69ca5698a538357cef4e536c38e0f751b978bd0a
Kadek Cahya <kdkchy@gmail.com>
2020-10-10 17:40:39 +0700

    api-simple

--- 08/run.py
+++ 08/run.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify, request, render_template
 from pymongo import MongoClient
 import os
+import json
 from bson.objectid import ObjectId
 
 app = Flask(__name__)
@@ -9,6 +10,13 @@ app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 mongodb_host = os.environ.get('MONGO_HOST', 'localhost')
 mongodb_port = int(os.environ.get('MONGO_PORT', '27017'))
 client = MongoClient(mongodb_host, mongodb_port)   
+
+class JSONEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, ObjectId):
+            return str(o)
+        return json.JSONEncoder.default(self, o)
+
 
 @app.route("/")
 def home():
@@ -89,6 +97,46 @@ def remove():
     db.person.remove({"_id":ObjectId(key)})
     return render_template(
                 'msg.html', my_string="Data Has Been Deleted.", title="Delete")
+
+@app.route("/api/users", methods=["GET", "POST"])
+def api():
+    db = client.rental 
+    if request.method == 'GET':
+        person = db.person.find()
+        result = []
+        for i in person:
+            result.append(i)
+        return JSONEncoder().encode(result)
+
+    if request.method == 'POST':
+        insert = request.get_json()
+        db.person.insert(insert)
+        return JSONEncoder().encode(insert)
+
+@app.route("/api/users/<id>", methods=["GET", "PUT", "DELETE"])
+def apinest(id):
+    db = client.rental 
+
+    if request.method == 'GET':
+        person = db.person.find({"_id" : ObjectId(id)})
+        result = []
+        for i in person:
+            result.append(i)
+        return JSONEncoder().encode(result)
+
+    if request.method == 'PUT':
+        insert=request.get_json()
+
+        db.person.update(
+            {"_id":ObjectId(id)},
+            {'$set':{"name" : insert['name'], "address" : insert['address']}
+            })
+        return JSONEncoder().encode(db.person.find_one(ObjectId(id)))
+
+    if request.method == 'DELETE':
+        db.person.remove({"_id":ObjectId(id)})
+
+        return {"result" : "ok"}  
 
 if __name__ == '__main__':
     app.run(debug=True)
